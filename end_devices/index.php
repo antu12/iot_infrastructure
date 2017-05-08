3fdcc23bf5be56f88965fd07bdbf6b03524526bd
Siratim Mustaquim <tanzim121@gmail.com>
2017-05-09 05:28:37 +0600

    end device request json banano

--- end_devices/index.php
+++ end_devices/index.php
@@ -4,21 +4,23 @@
 	$services = array('aqaas', 'haas','taas', 'waas','wfaas' );
 	$simple_services = array('aqaas', 'haas','taas','wfaas' );
 	$complex_services = 'waas';
+	$req;
 
 	//Service structure
 	$simple = array('service_id','token','value','expiration');
 	$complex = array('service_id', 'service_elements', 'token', 'default', 'expiration');
 
 	//Check Service
-	$service = $services[rand(0, sizeof($services)-1)]
+	$service = $services[rand(0, sizeof($services)-1)];
 	if (in_array($service, $simple_services)) {
-		
+		$reqService = array('service_id' => $service, 'components' => $simple);
+		$req = json_encode($reqService);
 	}else{
-
+		$reqService = array('service_id' => $service, 'components' => $complex);
+		$req = json_encode($reqService);
 	}
-	$reqService = array('service_id' => );
-	// echo $reqService['service_id'];
-	$req = json_encode($reqService);
+	
+	
 	echo $req;
 
  ?>
